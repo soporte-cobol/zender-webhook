84e61bb7a7054381129da214796d00e89164504b
soporte-cobol <129012735+soporte-cobol@users.noreply.github.com>
2026-04-28 19:51:56 -0500

    Add error tracking to passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,12 @@
-﻿import os
+import os
 import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app import app as application
+try:
+    from app import app as application
+except Exception as e:
+    import traceback
+    with open(os.path.join(os.path.dirname(__file__), 'startup_error.txt'), 'w') as f:
+        traceback.print_exc(file=f)
+    raise
